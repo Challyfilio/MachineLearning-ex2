78018795a7519e06160badf3d3b7fb8b41554a88
Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-10-09 14:52:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy.optimize as opt
 from sklearn.metrics import classification_report
-
+'''
 data = pd.read_csv('ex2data1.txt', names=['exam1', 'exam2', 'admitted'])
 # print(data.head())
 # sns.set(context="notebook", style="darkgrid", palette=sns.color_palette("RdBu", 2))
@@ -44,12 +44,12 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 
-'''
-fig, ax = plt.subplots(figsize=(8, 6))
-ax.plot(np.arange(-10, 10, step=0.01), sigmoid(np.arange(-10, 10, step=0.01)))
-ax.set_ylim(-0.1, 1.1)
-plt.show()
-'''
+
+# fig, ax = plt.subplots(figsize=(8, 6))
+# ax.plot(np.arange(-10, 10, step=0.01), sigmoid(np.arange(-10, 10, step=0.01)))
+# ax.set_ylim(-0.1, 1.1)
+# plt.show()
+
 theta = np.zeros(3)
 
 
@@ -89,6 +89,7 @@ print(res.x)
 # 寻找决策边界
 coef = -(res.x / res.x[2])
 print('coef', coef)
+
 x = np.arange(150, step=0.1)
 y = coef[0] + coef[1] * x
 
@@ -98,3 +99,10 @@ plt.plot(x, y, 'r')
 plt.xlim(27, 102)
 plt.ylim(27, 102)
 plt.show()
+'''
+data = pd.read_csv('ex2data2.txt', names=['test1', 'test2', 'accepted'])
+sns.set(context="notebook", style="darkgrid", palette=sns.color_palette("RdBu", 2))
+sns.lmplot(x='test1', y='test2', hue='accepted', data=data,
+            height=6, fit_reg=False, scatter_kws={"s": 50})  # fit_reg:是否显示拟合曲线
+plt.show()
+
