cc794499849ff1c662b90618ab27843ecab8e9fe
Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-10-11 11:15:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -60,13 +60,15 @@ plt.show()
 
 # Regularized logistic regression
 # data2
-data = pd.read_csv('ex2data2.txt', names=['test1', 'test2', 'accepted'])
-
-
-# sns.set(context="notebook", style="darkgrid", palette=sns.color_palette("RdBu", 2))
+data2 = pd.read_csv('ex2data2.txt', names=['test1', 'test2', 'accepted'])
 # sns.lmplot(x='test1', y='test2', hue='accepted', data=data,
 #            height=6, fit_reg=False, scatter_kws={"s": 50})  # fit_reg:是否显示拟合曲线
 # plt.show()
+
+x1 = np.array(data2.test1)
+x2 = np.array(data2.test2)
+y = get_y(data2)
+
 
 # 特征映射
 def feature_mapping(x, y, power, as_ndarray=False):
@@ -80,18 +82,22 @@ def feature_mapping(x, y, power, as_ndarray=False):
         return pd.DataFrame(data)
 
 
+power = 5
+x = feature_mapping(x1, x2, power)
+theta = np.zeros(x.shape[1])
+
+
 # 正则化代价
-def regularized_cost(theta, x, y, λ=λ):
+def Regularized_cost(theta, x, y, λ=λ):
     theta_j1_to_n = theta[1:]
     regularized_term = (λ / (2 * len(x))) * np.power(theta_j1_to_n, 2).sum()
     return cost(theta, x, y) + regularized_term
 
 
-# print(regularized_cost(theta, x, y, λ=λ))
-
+# print(Regularized_cost(theta, x, y, λ=λ))
 
 # 正则化梯度
-def regularized_gradient(theta, x, y, λ=λ):
+def Regularized_gradient(theta, x, y, λ=λ):
     theta_j1_to_n = theta[1:]  # 不加theta0
     regularized_theta = (λ / len(x)) * theta_j1_to_n
 
@@ -99,28 +105,17 @@ def regularized_gradient(theta, x, y, λ=λ):
     return gradientDescent(theta, x, y) + regularized_term
 
 
-# print(regularized_gradient(theta, x, y, λ=λ))
+# print(Regularized_gradient(theta, x, y, λ=λ))
+
+res = opt.minimize(fun=Regularized_cost, x0=theta, args=(x, y), method='TNC', jac=Regularized_gradient)
+final_theta = res.x
+
 
 # print('init cost ={}'.format(regularized_cost(theta, x, y, λ)))
 # res = opt.minimize(fun=regularized_cost, x0=theta, args=(x, y), method='Newton-CG', jac=regularized_gradient)
 # print(res)
 
-
-def feature_mapped_logistic_regression(power):
-    df = pd.read_csv('ex2data2.txt', names=['test1', 'test2', 'accepted'])
-    x1 = np.array(df.test1)
-    x2 = np.array(df.test2)
-    y = get_y(df)
-
-    x = feature_mapping(x1, x2, power)
-    theta = np.zeros(x.shape[1])
-
-    res = opt.minimize(fun=regularized_cost, x0=theta, args=(x, y), method='TNC', jac=regularized_gradient)
-    final_theta = res.x
-
-    return final_theta
-
-
+# 寻找决策边界
 def find_decision_boundary(density, power, theta, threshhold):
     t1 = np.linspace(-1, 1.5, density)
     t2 = np.linspace(-1, 1.5, density)
@@ -128,24 +123,21 @@ def find_decision_boundary(density, power, theta, threshhold):
     cordinates = [(x, y) for x in t1 for y in t2]
     x_cord, y_cord = zip(*cordinates)
     mapped_cord = feature_mapping(x_cord, y_cord, power)
-
     inner_product = mapped_cord.values.dot(theta)
     decision = mapped_cord[np.abs(inner_product) < threshhold]
-
     return decision.f10, decision.f01
 
 
-def draw_boundary(power, λ):
+def draw_boundary(power, final_theta, λ):
     density = 1000
     threshhold = 2 * 10 ** -3
-    final_theta = feature_mapped_logistic_regression(power)
+
     x, y = find_decision_boundary(density, power, final_theta, threshhold)
 
     df = pd.read_csv('ex2data2.txt', names=['test1', 'test2', 'accepted'])
     sns.lmplot(x='test1', y='test2', hue='accepted', data=df, height=6, fit_reg=False, scatter_kws={"s": 100})
-
     plt.scatter(x, y, c='r', s=10)
     plt.show()
 
 
-draw_boundary(power=5, λ=λ)
+draw_boundary(power, final_theta, λ)
