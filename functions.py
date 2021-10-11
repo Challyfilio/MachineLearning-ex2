0f06e66d0b110afb4e68dac276bd47950a90575e
Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-10-11 11:15:22 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 # 读取特征
@@ -36,3 +37,30 @@ def gradientDescent(theta, x, y):
 def predict(x, theta):
     prob = sigmoid(np.dot(x, theta))
     return (prob >= 0.5).astype(int)
+
+
+# 特征映射
+def feature_mapping(x, y, power, as_ndarray=False):
+    data = {"f{}{}".format(i - p, p): np.power(x, i - p) * np.power(y, p)
+            for i in np.arange(power + 1)
+            for p in np.arange(i + 1)
+            }
+    if as_ndarray:
+        return pd.DataFrame(data).as_matrix()
+    else:
+        return pd.DataFrame(data)
+
+
+# 正则化代价
+def Regularized_Cost(theta, x, y, λ):
+    theta_j1_to_n = theta[1:]
+    regularized_term = (λ / (2 * len(x))) * np.power(theta_j1_to_n, 2).sum()
+    return cost(theta, x, y) + regularized_term
+
+
+# 正则化梯度
+def Regularized_Gradient(theta, x, y, λ):
+    theta_j1_to_n = theta[1:]  # 不加theta0
+    regularized_theta = (λ / len(x)) * theta_j1_to_n
+    regularized_term = np.concatenate([np.array([0]), regularized_theta])
+    return gradientDescent(theta, x, y) + regularized_term
